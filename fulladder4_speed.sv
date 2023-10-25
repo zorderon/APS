6173cd1e67ba7df50ef7c1a7a9421ab5503c4243
zorderon <148991609+zorderon@users.noreply.github.com>
2023-10-25 18:39:47 +0300

    Update fulladder4_speed.sv

--- fulladder4_speed.sv
+++ fulladder4_speed.sv
@@ -22,7 +22,7 @@ module fulladder4_speed(
   assign g0 = a_i[2] & b_i[2];
   assign g0 = a_i[3] & b_i[3];
 
-  assign carry1 = g0 | (p0 & cin);
+  assign carry1 = g0 | (p0 & carry_i);
   assign carry2 = g1 | (p1 & g0) | (p1 & p0 & carry_i);
   assign carry3 = g2 | (p2 & g1) | (p2 & p1 & p0 & carry_i);
 	assign carry4 = g3 | (p3 & g2) | (p3 & p2 & g1) | (p3 & p2 & p1 & g0) | (p3 & p2 & p1 & p0 & carry_i);

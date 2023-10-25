ef8040b982b23be6faf452ee54270842361bf9c7
zorderon <148991609+zorderon@users.noreply.github.com>
2023-10-25 18:49:09 +0300

    fix: fix!

--- fulladder4_speed.sv
+++ fulladder4_speed.sv
@@ -14,17 +14,17 @@ module fulladder4_speed(
 
   assign p0 = a_i[0] ^ b_i[0];
   assign p1 = a_i[1] ^ b_i[1];
-  assign p1 = a_i[2] ^ b_i[2];
-  assign p1 = a_i[3] ^ b_i[3];
+  assign p2 = a_i[2] ^ b_i[2];
+  assign p3 = a_i[3] ^ b_i[3];
   
   assign g0 = a_i[0] & b_i[0];
-  assign g0 = a_i[1] & b_i[1];
-  assign g0 = a_i[2] & b_i[2];
-  assign g0 = a_i[3] & b_i[3];
+  assign g1 = a_i[1] & b_i[1];
+  assign g2 = a_i[2] & b_i[2];
+  assign g3 = a_i[3] & b_i[3];
 
   assign carry1 = g0 | (p0 & carry_i);
   assign carry2 = g1 | (p1 & g0) | (p1 & p0 & carry_i);
-  assign carry3 = g2 | (p2 & g1) | (p2 & p1 & p0 & carry_i);
+  assign carry3 = g2 | (p2 & g1) | (g0 & p1 & p2) | (p2 & p1 & p0 & carry_i);
 	assign carry4 = g3 | (p3 & g2) | (p3 & p2 & g1) | (p3 & p2 & p1 & g0) | (p3 & p2 & p1 & p0 & carry_i);
   
   assign sum_o[0] = p0 ^ carry_i;
@@ -32,9 +32,6 @@ module fulladder4_speed(
   assign sum_o[2] = p2 ^ carry2;
   assign sum_o[3] = p3 ^ carry3;
 
-	assign	carry_o = carry4;
-	endmodule
+  assign	carry_o = carry4;
     
-assign carry_o = carry4;
-
 endmodule
